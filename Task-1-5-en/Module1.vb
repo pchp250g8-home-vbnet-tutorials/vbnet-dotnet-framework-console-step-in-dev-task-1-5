6c61429df66776ce4c6ba09410901d992ce18af2
Alexander <pchp250g8@tutamail.com>
2026-09-15 19:24:16 +0300

    Fixed Logical Errors

--- Task-1-5-en/Module1.vb
+++ Task-1-5-en/Module1.vb
@@ -7,9 +7,9 @@
         UInteger.TryParse(Console.ReadLine(), k)
         Console.Write("How many pages remain to read? ")
         UInteger.TryParse(Console.ReadLine(), n)
-        Dim d = n \ k + n Mod k ' Number Of days the book will last
+        Dim d = (n + k - 1) \ k ' Number Of days the book will last
         ' Print to screen
-        Console.WriteLine($"This book will last you another {d} days.")
+        Console.WriteLine($"This book will last you another {d} day(s).")
         Console.Read() ' Press key "Enter" to close window
     End Sub
 
--- Task-1-5-ru/Module1.vb
+++ Task-1-5-ru/Module1.vb
@@ -7,7 +7,7 @@
         UInteger.TryParse(Console.ReadLine(), k)
         Console.Write("Сколько страниц осталось прочитать? ")
         UInteger.TryParse(Console.ReadLine(), n)
-        Dim d = n \ k + n Mod k ' Количество дней, на которое хватит книги
+        Dim d = (n + k - 1) \ k ' Количество дней, на которое хватит книги
         ' Вывод на экран
         Console.WriteLine($"Этой книги вам хватит ещё на {d} дня(ей).")
         Console.Read() ' Нажать клавишу "Enter" для закрытия окна
